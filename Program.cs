d68f75bcd98603116db815c44bd07e207e990236
adamBernat <59315587+adamBernat@users.noreply.github.com>
2020-01-27 17:29:20 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            string x = Console.ReadLine();
+            Console.WriteLine(x);
+            string text = "sss\"\"ss";
+
+
+            string y = Console.ReadLine();
+        }
+    }
+}
